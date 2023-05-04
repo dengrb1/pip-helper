35f48dc36bea612c1016677f8069e95300933bb6
dengrb1 <125752069+dengrb1@users.noreply.github.com>
2023-05-04 17:30:35 +0800

    update

--- update.py
+++ update.py
@@ -1,25 +1,19 @@
 from tkinter import *
 from tkinter import messagebox
-
+import webbrowser
 
 update = Tk()
 url_update = 'https://github.com/dengrb1/pygk'
 
 
 la_1 = Label(update, text='更新日志').pack()
-la_2 = Label(update, text='''1.0 程序制作完成
-1.2 程序新增pyqt5、pygame、pyinstaller安装
-1.3 程序加入更新界面，里面只显示更新的内容，不会自动更新！
-1.4 新增Requests库安装和删除
-1.5 新增wxpython库安装和删除
-1.6 加入关于模块
-1.7 新增一个小彩蛋
-1.8 新增nuitka安装和删除
-1.9 新增django库安装和删除
-2.0 修改窗口参数''').pack()
+la_2 = Label(update, text='''0.1.0 版本为beta and demo version''').pack()
 la_3 = Label(update, text='当前版本:2.0 (Not beta verison)')
 
 # 定义按钮
+def update_now():
+    webbrowser.open(url_update)
+    pass
 def quit_update():
     update.destroy()
     pass
