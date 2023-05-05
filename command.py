daf40f39adb7724e895072f4dcc1f8eddb9d7531
dengrb1 <125752069+dengrb1@users.noreply.github.com>
2023-05-05 12:24:01 +0800

    run_powershell

---
+++ command.py
@@ -0,0 +1,20 @@
+import os
+from time import sleep
+
+
+def run():
+    for i in range(4):
+        os.system('start powershell')
+        pass
+    pass
+
+
+# mainloop
+print('正在启动')
+sleep(0.666)
+print('Starting up')
+sleep(0.22222)
+run()
+
+# exit
+exit()
