95b969c899f94dc9881cbf5fde0c178752e5e84a
dengrb1 <125752069+dengrb1@users.noreply.github.com>
2023-05-05 12:18:46 +0800

    update

--- update.py
+++ update.py
@@ -1,14 +1,22 @@
 from tkinter import *
 from tkinter import messagebox
 import webbrowser
+from tkinter.scrolledtext import ScrolledText
 
 update = Tk()
 url_update = 'https://kgithub.com/dengrb1/pip-helper/releases'
 
 
 la_1 = Label(update, text='更新日志').pack()
-la_2 = Label(update, text='''0.1.0 版本为beta and demo version''').pack()
-la_3 = Label(update, text='当前版本:0.1.0 (beta verison)')
+text = """0.1.0 version is beta and demo version
+
+当前版本:0.1.0"""
+
+text_box = ScrolledText(root)
+text_box.pack(fill=BOTH, expand=1)
+text_box.insert(END, text)
+text_box.configure(state='disabled')
+
 
 # 定义按钮
 def update_now():
@@ -19,12 +27,9 @@ def quit_update():
     update.destroy()
     pass
 # 按钮设置
-quit_window_bt = Button(update, text='返回', command=quit_update)
-update_now_bt = Button(update, text='更新到最新版本', command=update_now)
+update_now_bt = Button(update, text='更新到最新版本', command=update_now).pack(side=RIGHT)
+quit_window_bt = Button(update, text='返回', command=quit_update).pack(side=RIGHT)
 
-# Button pack
-update_now_bt.pack()
-quit_window_bt.pack()
 
 # 初始化程序
 update.title('更新日志')
